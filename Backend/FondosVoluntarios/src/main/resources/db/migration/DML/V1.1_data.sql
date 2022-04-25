63a19ac8848cdbe8f381f3e84c3251ca0d1050f1
Giovanni Gomez Restrepo <ggomezr@sura.com.co>
2022-04-25 08:23:08 -0500

    Se genera insercion

---
+++ Backend/FondosVoluntarios/src/main/resources/db/migration/DML/V1.1_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO fondosvoluntarios.fondos(nombre_fondo, monto, categoria)
+VALUES ('FPV_BTG_PACTUAL_RECAUDADORA', 75000, 'FPV');
+
+INSERT INTO fondosvoluntarios.fondos(nombre_fondo, monto, categoria)
+VALUES ('FPV_BTG_PACTUAL_ECOPETROL', 125000, 'FPV');
+
+INSERT INTO fondosvoluntarios.fondos(nombre_fondo, monto, categoria)
+VALUES ('DEUDAPRIVADA', 50000, 'FIC');
+
+INSERT INTO fondosvoluntarios.fondos(nombre_fondo, monto, categoria)
+VALUES ('FDO-ACCIONES', 250000, 'FIC');
+
+INSERT INTO fondosvoluntarios.fondos(nombre_fondo, monto, categoria)
+VALUES ('FPV_BTG_PACTUAL_DINAMICA', 100000, 'FPV');
